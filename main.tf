2404023e9d7e9e97df678ce7d2947b64c24617de
Dan Fedick <daniel.fedick@hashicorp.com>
2023-04-05 16:55:47 -0400

    Update vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# locals {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -14,33 +15,33 @@ module "vpc" {
 
 }
 
-resource "aws_security_group" "ssh" {
-  name        = "ssh"
-  description = "Allow ssh inbound traffic"
-  vpc_id      = local.vpc_id
+# resource "aws_security_group" "ssh" {
+#   name        = "ssh"
+#   description = "Allow ssh inbound traffic"
+#   vpc_id      = local.vpc_id
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}"]
-  }
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["${var.my_ip}"]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  tags = {
-    Name = "allow_ssh"
-  }
-}
+#   tags = {
+#     Name = "allow_ssh"
+#   }
+# }
 
-resource "aws_key_pair" "management_key" {
-  key_name   = "management"
-  public_key = var.management_pubkey
-}
+# resource "aws_key_pair" "management_key" {
+#   key_name   = "management"
+#   public_key = var.management_pubkey
+# }
