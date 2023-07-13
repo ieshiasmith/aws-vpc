e5c627414d8164c3d044b054daad8e7942e37307
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-07-13 14:38:39 -0400

    Remove certs

--- dns.tf
+++ dns.tf
@@ -1,37 +1,29 @@
-# Get the route53 zone
+# # Get the route53 zone
 
-data "aws_route53_zone" "hashidemos" {
-  name         = "daniel-fedick.sbx.hashidemos.io"
-  private_zone = false
-}
+# data "aws_route53_zone" "hashidemos" {
+#   name         = "daniel-fedick.sbx.hashidemos.io"
+#   private_zone = false
+# }
 
-locals {
-  apps = ["consul", "vault", "nomad", "terraform", "boundary"]
-}
+# locals {
+#   apps = ["consul", "vault", "nomad", "terraform", "boundary"]
+# }
 
-resource "aws_acm_certificate" "app_certs" {
-  for_each = toset(local.apps)
+# resource "aws_acm_certificate" "app_certs" {
+#   for_each = toset(local.apps)
   
-  domain_name       = "${each.value}.daniel-fedick.sbx.hashidemos.io"
-  validation_method = "DNS"
+#   domain_name       = "${each.value}.daniel-fedick.sbx.hashidemos.io"
+#   validation_method = "DNS"
 
-  tags = {
-    Name = "${upper(each.value)}Certificate"
-  }
+#   tags = {
+#     Name = "${upper(each.value)}Certificate"
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-output "validation_options" {
-  value = local.validation_options
-}
-
-# /*
-#   "flatten" the loop by creating a combined list of maps, 
-#   with each map containing both the app name and its respective domain_validation_options value
-# */
 # locals {
 #   validation_options = flatten([
 #     for app in local.apps : [
@@ -45,20 +37,7 @@ output "validation_options" {
 #   ])
 # }
 
-# # Now use the local.validation_options to generate records for each cert validation
-# resource "aws_route53_record" "app_cert_validation" {
-#   for_each = { for vo in local.validation_options : "${vo.app}-${replace(vo.name, ".", "-")}" => vo }
 
-#   name    = each.value.name
-#   type    = each.value.type
-#   zone_id = data.aws_route53_zone.hashidemos.zone_id
-#   records = [each.value.record]
-#   ttl     = 60
-# }
-
-# resource "aws_acm_certificate_validation" "app_cert" {
-#   for_each = toset(local.apps)
-  
-#   certificate_arn         = aws_acm_certificate.app_certs[each.key].arn
-#   validation_record_fqdns = [for record in aws_route53_record.app_cert_validation : record.fqdn if contains(record.key, each.key)]
+# output "validation_options" {
+#   value = local.validation_options
 # }
