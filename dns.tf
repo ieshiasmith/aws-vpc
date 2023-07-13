585485a1d306034993250f4bfcc6d6372287c53e
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-07-13 12:29:18 -0400

    DNS Updates for multiple apps

--- dns.tf
+++ dns.tf
@@ -5,37 +5,44 @@ data "aws_route53_zone" "hashidemos" {
   private_zone = false
 }
 
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = "vault.daniel-fedick.sbx.hashidemos.io"
-#   validation_method = "DNS"
+locals {
+  apps = ["consul", "vault", "nomad", "terraform", "boundary"]
+}
 
-#   tags = {
-#     Name = "VaultCertificate"
-#   }
+resource "aws_acm_certificate" "app_certs" {
+  for_each = toset(local.apps)
+  
+  domain_name       = "${each.value}.daniel-fedick.sbx.hashidemos.io"
+  validation_method = "DNS"
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  tags = {
+    Name = "${upper(each.value)}Certificate"
+  }
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_route53_record" "cert_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+resource "aws_route53_record" "app_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.app_certs[each.key].domain_validation_options : "${each.key}-${dvo.domain_name}" => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   name    = each.value.name
-#   type    = each.value.type
-#   zone_id = data.aws_route53_zone.hashidemos.zone_id
-#   records = [each.value.record]
-#   ttl     = 60
-# }
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = data.aws_route53_zone.hashidemos.zone_id
+  records = [each.value.record]
+  ttl     = 60
+}
 
-# resource "aws_acm_certificate_validation" "cert" {
-#   certificate_arn         = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "app_cert" {
+  for_each = toset(local.apps)
+  
+  certificate_arn         = aws_acm_certificate.app_certs[each.key].arn
+  validation_record_fqdns = [for record in aws_route53_record.app_cert_validation : record.fqdn if contains(record.key, each.key)]
+}
