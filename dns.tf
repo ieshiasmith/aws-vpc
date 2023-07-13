e6618a3594470807e1588df25c17a9529ee3fe8f
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-07-13 15:04:59 -0400

    Add in aws_acm_certs

--- dns.tf
+++ dns.tf
@@ -1,42 +1,42 @@
-# # Get the route53 zone
+# Get the route53 zone
 
-# data "aws_route53_zone" "hashidemos" {
-#   name         = "daniel-fedick.sbx.hashidemos.io"
-#   private_zone = false
-# }
+data "aws_route53_zone" "hashidemos" {
+  name         = "daniel-fedick.sbx.hashidemos.io"
+  private_zone = false
+}
 
-# locals {
-#   apps = ["consul", "vault", "nomad", "terraform", "boundary"]
-# }
+locals {
+  apps = ["consul", "vault", "nomad", "terraform", "boundary"]
+}
 
-# resource "aws_acm_certificate" "app_certs" {
-#   for_each = toset(local.apps)
+resource "aws_acm_certificate" "app_certs" {
+  for_each = toset(local.apps)
   
-#   domain_name       = "${each.value}.daniel-fedick.sbx.hashidemos.io"
-#   validation_method = "DNS"
+  domain_name       = "${each.value}.daniel-fedick.sbx.hashidemos.io"
+  validation_method = "DNS"
 
-#   tags = {
-#     Name = "${upper(each.value)}Certificate"
-#   }
+  tags = {
+    Name = "${upper(each.value)}Certificate"
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# locals {
-#   validation_options = flatten([
-#     for app in local.apps : [
-#       for dvo in aws_acm_certificate.app_certs[app].domain_validation_options : {
-#         app    = app
-#         name   = dvo.resource_record_name
-#         record = dvo.resource_record_value
-#         type   = dvo.resource_record_type
-#       }
-#     ]
-#   ])
-# }
+locals {
+  validation_options = flatten([
+    for app in local.apps : [
+      for dvo in aws_acm_certificate.app_certs[app].domain_validation_options : {
+        app    = app
+        name   = dvo.resource_record_name
+        record = dvo.resource_record_value
+        type   = dvo.resource_record_type
+      }
+    ]
+  ])
+}
 
-# output "validation_options" {
-#   value = local.validation_options
-# }
+output "validation_options" {
+  value = local.validation_options
+}
