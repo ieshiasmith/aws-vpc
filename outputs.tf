693f6ebb39e0b6b88c7cf3bff9f279fa2a00f4b2
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-11-15 12:09:42 -0500

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -39,13 +39,13 @@ output "vault_acm_cert" {
   value = aws_security_group.ssh.id
 }
 
-output "consul_secrets_manager_arn" {
-  value = aws_secretsmanager_secret.consul_key.arn
-}
+# output "consul_secrets_manager_arn" {
+#   value = aws_secretsmanager_secret.consul_key.arn
+# }
 
-output "consul_aws_kms_key_arn" {
-  value = aws_kms_key.consul.arn
-}
+# output "consul_aws_kms_key_arn" {
+#   value = aws_kms_key.consul.arn
+# }
 
 # # Output for the IAM role
 # output "hashistack_role_arn" {
