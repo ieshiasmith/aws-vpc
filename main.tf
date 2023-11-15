828db1c9206d3aab2a20884f6d6f30ee59cc0f4b
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-11-15 12:39:13 -0500

    consul key

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ module "vpc" {
 
 }
 
-resource "aws_secretsmanager_secret" "consul_key" {
-  name = "consul_key"
+resource "aws_secretsmanager_secret" "consul" {
+  name = "consul"
 }
 
 # resource "aws_kms_key" "consul_key" {
